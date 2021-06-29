cb98b986e78017df79635e74d4539631fb330de6
osias10 <wnsguddk1@naver.com>
2021-06-29 17:32:53 +0900

    익명채팅 기능 추가

--- src/utils/chatutils.js
+++ src/utils/chatutils.js
@@ -1,5 +1,5 @@
 function messageAnonymous(msg){
-    let chat = msg.content.substring(1);
+    let chat = msg.content.substring(3);
     msg.delete()
     .then (msg=> console.log(`익명채팅: [${msg.author.username}]\n 내용: ${msg.content}`))
     .catch(err => msg.channel.send("권한이 부족합니다"))
